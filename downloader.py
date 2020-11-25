ab39692e2937831c45700829f0b7f8f96262022f
aniket <dixitaniket199@gmail.com>
2020-11-25 22:09:42 +0530

    added downloading

--- downloader.py
+++ downloader.py
@@ -1,14 +1,17 @@
 from pytube import YouTube
 import sys
-endpoint=sys.argv[1]
-try:
-    yt_obj=YouTube(endpoint)
-    internal_stream = yt_obj.streams.filter(only_audio=True,file_extension="webm")[-1]
-    print(internal_stream)
-    internal_stream.download("/home/aniket/Music")
-except Exception as e:
-    print(e)
-
-print("thanks for using the service")
+def internal_youtube_routine(endpoint):
+    try:
+        yt_obj=YouTube(endpoint)
+        print(yt_obj)
+        internal_stream = yt_obj.streams.filter(only_audio=True,file_extension="webm")[-1]
+        
+        print("downloading")
+        internal_stream.download()
+        return yt_obj.title
+        
+    except Exception as e:
+        print(e)
+        return False
 
 
--- tele.py
+++ tele.py
@@ -1,24 +1,58 @@
 import telebot
+from downloader import internal_youtube_routine
+import os
 
 bot =telebot.TeleBot("1407230169:AAHmqDu4wCjp6Cl46Ipo_8l6qKobUOXr5z0", parse_mode=None)
+
 
 @bot.message_handler(commands=["start"])
 def send_message(message):
     #message.chat.id for the chat id
     bot.reply_to(message,"welcome to mybot, please enter the url of the youtube music video")
-    #chat_id=message.chat.id
-    
-    #audio=open("/home/aniket/Music/Lil Nas X - Panini (Official Video).webm","rb")
-    #1bot.send_audio(chat_id,audio)
-
-
-
-@bot.message_handler(comamnds=["sendmusic"])
-def sending_audio_testing(message):
-    bot.reply_to()
     chat_id=message.chat.id
-    audio=open("/home/aniket/Music/Lil Nas X - Panini (Official Video).webm","rb")
-    bot.send_audio(chat_id,audio)
+    print(chat_id)
+    bot.reply_to(message,"hello")
+    #audio=open("/home/aniket/Music/Dylan Sitts - So Free.webm","rb")
+    #bot.send_audio(chat_id,audio)
+
+
+
+@bot.message_handler(commands=["sendmusic"])
+def sending_audio_testing(message):   	
+    bot.reply_to(message,"paste in the youtube url")
+
+
+@bot.message_handler(commands=["future"])
+def internal_future(message):
+    bot.reply_to(message,"currently working on downloading videos directly from youtube to your chat")
+
+@bot.message_handler(commands=["about"])
+def internal_about(message):
+    bot.reply_to("bot made by someone you know")
+
+def url_checker(message):
+    if (message.entities==None):
+        return False
+    else:
+        if (message.entities[0].type=="url"):
+            return True
+        else:
+            return False
+
+
     
+@bot.message_handler(func=url_checker)
+def internal_url_handler(message):
+    print(message.entities[0].type)
+    bot.reply_to(message,"sending audio to chat")
+    status=internal_youtube_routine(message.text)
+    if (status==False):
+        bot.reply_to(message,"sorry some error occured while downloading")
+    else:
+        audio=open(status+".webm","rb")
+        bot.send_audio(message.chat.id,audio)
+        bot.reply_to(message,"delivered boss")
+        os.remove(status+".webm")
+
 
 bot.polling()
