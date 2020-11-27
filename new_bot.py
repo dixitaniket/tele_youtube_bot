45a868e055b11c2160c993e7a6bac3259f99b0a0
aniket <aniket@pop-os.localdomain>
2020-11-27 18:16:01 +0530

    second library

--- new_bot.py
+++ new_bot.py
@@ -4,8 +4,10 @@ from telegram.ext import CommandHandler,MessageHandler
 from telegram.ext.filters import Filters
 from downloader import internal_youtube_routine
 import os
+# import sqlite3
 
-updater =Updater(token="1407230169:AAHmqDu4wCjp6Cl46Ipo_8l6qKobUOXr5z0",use_context=True)
+# conn=sqlite3.connect("userList.db")
+updater =Updater(token="1407230169:AAHmqDu4wCjp6Cl46Ipo_8l6qKobUOXr5z0",use_context=True,workers=2)
 
 dispatcher=updater.dispatcher
 
@@ -15,6 +17,7 @@ def start(update,context):
 
 def internal_download(update,context):
     # print(update.text)
+    username=update.message.from_user.first_name
     if (update.message.via_bot!=None):
         context.bot.send_message(chat_id=update.effective_chat.id,text="we Do not need music")
     else:
@@ -22,24 +25,30 @@ def internal_download(update,context):
         if (type(endpoint)==type("")):
             filename=internal_youtube_routine(endpoint)
             if filename==False:
-                context.bot.send_message(update.message.chat_id,"some error occured")
-            audio = open(filename,"rb")
-            context.bot.send_audio(update.message.chat_id,audio)
-            context.bot.send_message (update.message.chat_id,"task complete")
-            audio.close()            
-            os.remove(filename)
+                context.bot.send_message(update.message.chat_id,f"some error occured {username}")
+           
+            try:
+                audio = open(filename,"rb")
+                
+                context.bot.send_message(update.message.chat_id,f"Task in Progress {username}")
+                context.bot.send_audio(update.message.chat_id,audio)
+                context.bot.send_message(update.message.chat_id,f"Task Completed {username}")
+                audio.close()            
+                os.remove(filename)
+            except Exception as e:
+                context.bot.send_message(update.message.chat_id,f"Some error occured {username}")
         else:
-            context.bot.send_message(update.message.chat_id,"some error occured")
+            context.bot.send_message(update.message.chat_id,f"Some error occured {username}")
 
 
 
 
 start_handler= CommandHandler("start",start)
-message_handler=MessageHandler(Filters.entity("url"),internal_download)
+message_handler=MessageHandler(Filters.entity("url"),internal_download,run_async=True)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(message_handler)
 
 
-updater.start_polling()
+updater.start_polling(poll_interval=0.5,read_latency=0.5)
 
